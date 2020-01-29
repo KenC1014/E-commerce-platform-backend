d5d1569f6bebccb9f2c3111ae9b341d453ae7b62
Ken Chen <kenken4016@gmail.com>
2020-01-29 09:46:16 -0500

    modified dev scripts and moved app.js

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-require('./db/mongoose')   //connect to mongoDB
-
+require('./src/db/mongoose')   //connect to mongoDB
 
 //import routers
-const authRouter = require('./routers/auth')
-const userRouter = require('./routers/user')
-const categoryRouter = require('./routers/category')
-const productRouter = require('./routers/product')
-const braintreeRouter = require('./routers/braintree')
-const orderRouter = require('./routers/order')
+const authRouter = require('./src/routers/auth')
+const userRouter = require('./src/routers/user')
+const categoryRouter = require('./src/routers/category')
+const productRouter = require('./src/routers/product')
+const braintreeRouter = require('./src/routers/braintree')
+const orderRouter = require('./src/routers/order')
 
-//seeting up express server
+//set up express server
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
